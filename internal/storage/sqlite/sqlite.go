e017b3738777999b87b4a893423beefc0e5abf10
Roman Timoshkin <rmntim@yandex.ru>
2024-03-08 12:39:12 +0300

    feat: add basic CRUD operations for urls

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,9 +2,12 @@ package sqlite
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3" // init sqlite3 driver
+	"github.com/rmntim/go-url-shortener/internal/storage"
 )
 
 type Storage struct {
@@ -39,4 +42,79 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+// SaveURL adds new URL to storage. Returns ID of new URL
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES (?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+// Gets URL by alias. Returns ErrURLNotFound if URL doesn't exist.
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+
+	stmt, err := s.db.Prepare("SELECT url FROM url WHERE alias = ?")
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	var urlGot string
+	err = stmt.QueryRow().Scan(&urlGot)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", storage.ErrURLNotFound
+		}
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return urlGot, nil
+}
+
+// DeleteURL returns ErrURLNotFound if you tried to delete url that doesn't exist.
+// For example when you delete same thing twice.
+func (s *Storage) DeleteURL(alias string) error {
+  const op = "storage.sqlite.DeleteURL"
+
+  stmt, err := s.db.Prepare("DELETE FROM url WHERE alias = ?")
+  if err != nil {
+    return fmt.Errorf("%s: %w", op, err)
+  }
+
+  res, err := stmt.Exec()
+  if err != nil {
+    return fmt.Errorf("%s: %w", op, err)
+  }
+
+  rows, err := res.RowsAffected()
+  if err != nil {
+    return fmt.Errorf("%s: delete failed: %w", op, err)
+  }
+
+  if rows == 0 {
+    return storage.ErrURLNotFound
+  }
+
+  return nil
 }
