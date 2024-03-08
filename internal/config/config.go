4ddb2af8cb63989aac8bf1f53ebf1555c38ead2c
Roman Timoshkin <rmntim@yandex.ru>
2024-03-08 10:35:24 +0300

    feat: add config.Load func

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"flag"
 	"os"
 	"time"
@@ -21,21 +22,29 @@ type HTTPServerConfig struct {
 }
 
 func MustLoad() *Config {
+  config, err := Load()
+  if err != nil {
+    panic(err)
+  }
+  return config
+}
+
+func Load() (*Config, error) {
 	configPath := fetchConfigPath()
 	if configPath == "" {
-		panic("config path is empty")
+    return nil, errors.New("config path is empty")
 	}
 
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		panic("config file doesn't exist: " + configPath)
+    return nil, errors.New("config file doesn't exist: " + configPath)
 	}
 
 	var config Config
 	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
-		panic("failed to read config: " + err.Error())
+    return nil, errors.New("failed to read config: " + err.Error())
 	}
 
-	return &config
+  return &config, nil
 }
 
 func fetchConfigPath() string {
