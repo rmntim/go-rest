e248a5b06c1f3e3c45de611a1135e8bece1bce8d
Roman Timoshkin <rmntim@yandex.ru>
2024-03-08 09:55:42 +0300

    feat: add main cmd

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+  // TODO: init config
+  // TODO: init logger
+  // TODO: init db
+  // TODO: init router
+  // TODO: init server
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmntim/go-url-shortener
+
+go 1.22.1
