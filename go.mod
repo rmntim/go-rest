8c07655380c491be7580b1f7c8f66b2e794c10f2
Roman Timoshkin <rmntim@yandex.ru>
2024-03-09 11:20:16 +0300

    misc: prettify

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/rmntim/go-url-shortener
 go 1.22.1
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/render v1.0.3
+	github.com/go-playground/validator/v10 v10.19.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/mattn/go-sqlite3 v1.14.22
 )
@@ -11,11 +14,8 @@ require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
-	github.com/go-chi/render v1.0.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
--- internal/lib/random/random.go
+++ internal/lib/random/random.go
@@ -12,20 +12,21 @@ const (
 	letterIdxMask = 1<<letterIdxBits - 1
 	letterIdxMax  = 63 / letterIdxBits
 )
+
 var src = rand.NewSource(time.Now().UnixNano())
 
 // NewRandomString uses some smart masking tomfoolery,
-// that I don't really understand, but (as a good developer) I can provide the source 
+// that I don't really understand, but (as a good developer) I can provide the source
 // https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
 func NewRandomString(n int) string {
-  sb := strings.Builder{}
-  sb.Grow(n)
+	sb := strings.Builder{}
+	sb.Grow(n)
 	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(symbols) {
-      sb.WriteByte(symbols[idx])
+			sb.WriteByte(symbols[idx])
 			i--
 		}
 		cache >>= letterIdxBits
